ab4690351397c93d92c7df443911e58865f598fa
VishnuChowdhary <112419086+VishnuChowdhary@users.noreply.github.com>
2023-03-25 14:49:50 +0530

    Final Commit in 1.0.2

--- drfecommerce/product/models.py
+++ drfecommerce/product/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from mptt.models import MPTTModel, TreeForeignKey
+from .fields import OrderField
 
 class ActiveQuerySet(models.QuerySet):
     # def get_queryset(self):
@@ -45,4 +47,16 @@ class ProductLine(models.Model):
     stock_qty = models.IntegerField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_line")
     is_active = models.BooleanField(default=False)
+    order = OrderField(unique_for_field="product", blank=True)
+    objects = ActiveQuerySet.as_manager()
+    
+    def clean_fields(self, exclude:None):
+        super().clean_fields(exclude=exclude)
+        qs = ProductLine.objects.filter(product=self.product)
+        for obj in qs:
+            if self.id != obj.id and self.order == obj.order:
+                raise ValidationError("Duplicate Value.")
+    
+    def __str__(self):
+        return str(self.order)
     
